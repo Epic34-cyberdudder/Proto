c18e9908459292a8bacc17602bf6a2a2765c4b34
KevinMayBeHere <geeteeoo@icloud.com>
2026-08-10 10:57:50 -0700

    Add Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,33 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Discord AI Bot', // Change 'My Docs' to your desired title here!
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Epic34-cyberdudder/Discord-AI-BOT' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Installation Guide', slug: 'guides/installation' },
-						{ label: 'Configuration Guide', slug: 'guides/configuration' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  site: 'https://epic34-cyberdudder.github.io',
+  base: '/Discord-AI-BOT',
+
+  integrations: [
+    starlight({
+      title: 'Discord AI Bot',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/Epic34-cyberdudder/Discord-AI-BOT',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Installation Guide', slug: 'guides/installation' },
+            { label: 'Configuration Guide', slug: 'guides/configuration' },
+          ],
+        },
+        {
+          label: 'Reference',
+          items: [{ autogenerate: { directory: 'reference' } }],
+        },
+      ],
+    }),
+  ],
 });
